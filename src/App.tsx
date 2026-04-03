f9eb7ea2782a49ccbf1f269db3f32882ad160629
DEV-TYY <suontii2023@gmail.com>
2026-04-04 00:47:06 +0700

    Add Dock.jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navbar, Welcome } from "#components"
+import { Navbar, Welcome, Dock } from "#components"
 
 function App() {
    
@@ -7,6 +7,7 @@ function App() {
    <main>
        <Navbar/>
        <Welcome/>
+       <Dock/>
    </main>
   )
 }

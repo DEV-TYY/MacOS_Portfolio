3d20b3c69f9742b22b3596d89a8f12cd347b72f4
DEV-TYYgit config --global user.name DEV-TYY <suontii2023@gmail.com>
2026-05-01 00:45:58 +0700

    Make skills

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import { Navbar, Welcome, Dock } from "#components"
+import { Navbar, Welcome, Dock } from "#components";
+import gsap from "gsap";
+import { Draggable } from "gsap/Draggable";
+import { Terminal } from "#windows";
+gsap.registerPlugin(Draggable);
 
 function App() {
    
-
   return (
    <main>
        <Navbar/>
        <Welcome/>
        <Dock/>
+
+       <Terminal />
    </main>
   )
 }
